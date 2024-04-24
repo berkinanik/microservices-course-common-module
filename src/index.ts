a1f32e0165d83c1434fd2c22acf86ab2abab4503
Berkin Anik <berkinanik@gmail.com>
2024-04-25 00:14:05 +0200

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 255,
+  blue: 0,
+  green: 0,
+};
+
+console.log(color);
+
+export default color;
